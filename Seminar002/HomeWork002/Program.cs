7a6434405a42c04e8f8f95abdb86b44f67402106
DmitryDW <DmitryDW1@yandex.ru>
2022-12-21 00:12:09 +0600

    Update HomeWork002

--- Seminar002/HomeWork002/Program.cs
+++ Seminar002/HomeWork002/Program.cs
@@ -2,35 +2,40 @@
 //число и на выходе показывает вторую цифру этого числа.
 
 /*
-int AverageFigure(int num)
+int AverageFigure(int num) 
 {
     return (num % 100) / 10;
 }
-Console.Write("Input three - digit number: ");
+Console.Write("Input three-digit number: ");
 int num = Convert.ToInt32(Console.ReadLine());
-Console.Write(AverageFigure(num));
+if (num > 99 & num < 1000) Console.Write($"Second digit " + AverageFigure(num));
+else if (num < -99 & num > -1000) 
+{
+    num = num * (-1);
+    Console.Write($"Second digit " + AverageFigure(num));
+}
+else Console.Write("Sorry, there is no three-digit number");
 */
 
 //Задача 13: Напишите программу, которая выводит третью цифру заданного 
 //числа или сообщает, что третьей цифры нет.
 
-
+/*
 int ThirdDigit(int num)
 {
     while (num >= 1000) num = num / 10;
     return num % 10;
 }
-int NegativeThirdDigit(int num)
-{
-    while (num <= 1000) num = num / 10;
-    return num & 10;
-}
 Console.Write("Input number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 if (num > 99) Console.Write(ThirdDigit(num));
-else if (num < -99) Console.Write(NegativeThirdDigit(num));
+else if (num < -99) 
+{
+    num = num * (-1);
+    Console.Write(ThirdDigit(num));
+}
 else Console.Write("Sorry, there is no third digit in your number");
-
+*/
 
 //Задача 15: Напишите программу, которая принимает на вход цифру, 
 //обозначающую день недели, и проверяет, является ли этот день выходным.
