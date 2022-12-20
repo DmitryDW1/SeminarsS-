9080de3f04686359be8aebb9980d88e6c21ad118
DmitryDW <DmitryDW1@yandex.ru>
2022-12-20 23:43:27 +0600

    Create HomeWork002

---
+++ Seminar002/HomeWork002/Program.cs
@@ -0,0 +1,52 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное 
+//число и на выходе показывает вторую цифру этого числа.
+
+/*
+int AverageFigure(int num)
+{
+    return (num % 100) / 10;
+}
+Console.Write("Input three - digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write(AverageFigure(num));
+*/
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного 
+//числа или сообщает, что третьей цифры нет.
+
+
+int ThirdDigit(int num)
+{
+    while (num >= 1000) num = num / 10;
+    return num % 10;
+}
+int NegativeThirdDigit(int num)
+{
+    while (num <= 1000) num = num / 10;
+    return num & 10;
+}
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 99) Console.Write(ThirdDigit(num));
+else if (num < -99) Console.Write(NegativeThirdDigit(num));
+else Console.Write("Sorry, there is no third digit in your number");
+
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+/*
+bool Weekend(int num)
+{
+    if (num >= 1 & num <=7)
+    {
+        if (num >1 & num <=5) return false;
+        else return true;
+    }
+    else return false;
+}
+Console.Write("Input the digit of the day of the week. It's a weekend?: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 1 || num > 7) Console.Write("It's not a day of the week");
+else Console.Write(Weekend(num));
+*/
